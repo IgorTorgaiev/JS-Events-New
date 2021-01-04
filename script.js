fb8df549c66b672a54c797bd8889b852fbab4123
Igor Torgaiev <Itorgaiev@gmail.com>
2021-01-04 13:25:38 +0100

    JavaScript: Events (new)

---
+++ script.js
@@ -0,0 +1,8 @@
+function theFunction() {
+    alert("After every key press here you will see a notification");
+}
+
+function myFunction(){
+    a = document.getElementById("selection").value;
+    document.getElementById("demo3").innerHTML = "You can choose: " + a;
+}
